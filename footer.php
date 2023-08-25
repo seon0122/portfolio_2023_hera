bc48ce12c972b219e0f9f0c6218dcbb145776e07
miseon <bunny62420@gmail.com>
2023-08-25 18:11:24 +0900

    product

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="f_top">
+        <div class="left">
+            <a href="#">서비스이용약관</a>
+            <a href="#">UGC이용약관</a>
+            <a href="#">개인정보처리방침</a>
+            <a href="#">뷰티포인트</a>
+            <a href="#">사이트맵</a>
+        </div>
+        <div class="right">
+            <a href="#" title="페이스북"><img src="./images/png/facebook.png" alt=""></a>
+            <a href="#" title="유튜브"><img src="./images/png/youtube.png" alt=""></a>
+            <a href="#" title="인스타그램"><img src="./images/png/instagram.png" alt=""></a>
+            <a href="#" title="카카오톡"><img src="./images/png/kakaotalk.png" alt=""></a>
+        </div>
+    </div>
+    <p>고객상담팀(수신자요금부담) 080-023-5454 (상담시간 월~금 : AM 09:00 ~ PM 06:00)</p>
+    <div class="f_btm">
+        <p>© AMOREPACIFIC CORPORATION. All rights reserved.</p>
+        <a href="#"><img src="./images/web-accessibility_kr_230510.png" alt=""></a>
+    </div>
+</footer>
